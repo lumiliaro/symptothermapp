41af9d3aefae29e8a0244497e6120b12ade5ebe5
Lukas <roehrlukas@icloud.com>
2024-08-24 21:39:42 +0200

    fixed eslint & tsconfig

--- src-ui/eslint.config.js
+++ src-ui/eslint.config.js
@@ -5,40 +5,43 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
-    settings: { react: { version: "18.3" } },
-    ignores: ["dist"],
-    extends: [
-        js.configs.recommended,
-        ...tseslint.configs.recommendedTypeChecked,
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-        parserOptions: {
-            project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-            tsconfigRootDir: import.meta.dirname,
-        },
-    },
-    plugins: {
-        react,
-        "react-hooks": reactHooks,
-        "react-refresh": reactRefresh,
-    },
-    rules: {
-        ...reactHooks.configs.recommended.rules,
-        "react-refresh/only-export-components": [
-            "warn",
-            { allowConstantExport: true },
+export default tseslint.config(
+    { ignores: ["dist", "openapi-config.ts"] },
+    {
+        settings: { react: { version: "18.3" } },
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
         ],
-        ...react.configs.recommended.rules,
-        ...react.configs["jsx-runtime"].rules,
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-unsafe-assignment": "warn",
-        "@typescript-eslint/no-unsafe-argument": "warn",
-        "@typescript-eslint/no-unsafe-member-access": "warn",
-        "@typescript-eslint/no-unsafe-call": "warn",
-    },
-});
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+            ...react.configs.recommended.rules,
+            ...react.configs["jsx-runtime"].rules,
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-unsafe-assignment": "warn",
+            "@typescript-eslint/no-unsafe-argument": "warn",
+            "@typescript-eslint/no-unsafe-member-access": "warn",
+            "@typescript-eslint/no-unsafe-call": "warn",
+            "@typescript-eslint/no-misused-promises": "warn",
+        },
+    }
+);
