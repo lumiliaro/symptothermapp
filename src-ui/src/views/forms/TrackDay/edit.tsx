6849c913d5e3198d15440bcd5660b6788fa3a3ef
Lukas <roehrlukas@icloud.com>
2024-08-24 21:40:24 +0200

    fixed form.handleSubmit not triggering submit anymore

--- src-ui/src/views/forms/TrackDay/edit.tsx
+++ src-ui/src/views/forms/TrackDay/edit.tsx
@@ -77,8 +77,8 @@ export default function TrackDayEdit(props: { data: TrackDay }) {
 
     return (
         <FormProvider {...form}>
-            <form onSubmit={void form.handleSubmit(onSubmit)}>
-                <TrackDayView formType="edit" onDelete={void onDelete} />
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <TrackDayView formType="edit" onDelete={onDelete} />
             </form>
         </FormProvider>
     );
