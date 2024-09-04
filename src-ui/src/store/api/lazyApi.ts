4025cdb137ee2027125bbf5a7be88d7053399454
Lukas <roehrlukas@icloud.com>
2024-09-04 19:13:27 +0200

    removed unused query

--- src-ui/src/store/api/lazyApi.ts
+++ src-ui/src/store/api/lazyApi.ts
@@ -3,6 +3,5 @@ import { api } from "./generatedApi";
 export const {
     useLazyGetTrackDayByDateQuery,
     useLazyGetTrackDaysByMonthAndYearQuery,
-    useLazyGetTrackDaysStatisticByMonthAndYearQuery,
     useLazyGetCyclusStatisticByIdQuery,
 } = api;
