a3efbbc9b5593d9e31eb5b2f839b28b49df0e800
Lukas <roehrlukas@icloud.com>
2024-09-26 17:26:45 +0200

    fix: scripts

--- src-ui/scripts/copyChangelogMarkdown.js
+++ src-ui/scripts/copyChangelogMarkdown.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const sourceFile = path.join(__dirname, "CHANGELOG.md");
-const targetDir = path.join(__dirname, "src", "public");
+const targetDir = path.join(__dirname, "public");
 const targetFile = path.join(targetDir, "CHANGELOG.md");
 
 async function copyChangelog() {
